557b575d9087275ba4e847a28aff937db8e92c6e
Lukas Vann Struth <lvannstruth@gmail.com>
2017-10-04 19:14:30 -0700

    Initial Commit

---
+++ CSV File Creator/csvfileconverter.py
@@ -0,0 +1,18 @@
+import csv
+import sys
+
+
+#select data file from sd card
+#TODO figure out exactly how i'm structuring each sd card
+TEMPERATURE_FILE = open('put/in/proper/path')
+ALTITUDE_FILE = open('put/in/proper/path')
+ACCELERATION_FILE = open('put/in/proper/path')
+PRESSURE_FILE = open('put/in/proper/path')
+
+ALTITUDE_DATA =  []
+TEMPERATURE_DATA = []
+
+
+for line in DATA_FILE:
+    if line == :
+        pass
---
+++ Launch Sketch/skyprobelaunch.ino
@@ -0,0 +1,256 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP085_U.h>
+#include <Adafruit_ADXL345_U.h>
+
+/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
+   which provides a common 'type' for sensor data and some helper functions.
+   
+   To use this driver you will also need to download the Adafruit_Sensor
+   library and include it in your libraries folder.
+
+   You should also assign a unique ID to this sensor for use with
+   the Adafruit Sensor API so that you can identify this particular
+   sensor in any data logs, etc.  To assign a unique ID, simply
+   provide an appropriate value in the constructor below (12345
+   is used by default in this example).
+   
+   Connections
+   ===========
+   Connect SCL to analog 5
+   Connect SDA to analog 4
+   Connect VDD to 3.3V DC
+   Connect GROUND to common ground
+    
+   History
+   =======
+   2013/JUN/17  - Updated altitude calculations (KTOWN)
+   2013/FEB/13  - First version (KTOWN)
+*/
+   
+Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+
+
+/*************************/
+/* These offsets are used to calculate the acceleration, as the sensor can sometimes display erroneous information
+If the serial monitor is showing
+
+*/
+float xoffset = 0;
+float yoffset = 0.63;
+float zoffset = -9.65;
+
+/**************************************************************************/
+/*
+    Displays some basic information on this sensor from the unified
+    sensor API sensor_t type (see Adafruit_Sensor for more information)
+*/
+/**************************************************************************/
+void displaySensorDetails(void)
+{
+  sensor_t bmpsensor;
+  bmp.getSensor(&bmpsensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(bmpsensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(bmpsensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(bmpsensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(bmpsensor.max_value); Serial.println(" hPa");
+  Serial.print  ("Min Value:    "); Serial.print(bmpsensor.min_value); Serial.println(" hPa");
+  Serial.print  ("Resolution:   "); Serial.print(bmpsensor.resolution); Serial.println(" hPa");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  sensor_t accelsensor;
+  accel.getSensor(&accelsensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(accelsensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(accelsensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(accelsensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(accelsensor.max_value); Serial.println(" m/s^2");
+  Serial.print  ("Min Value:    "); Serial.print(accelsensor.min_value); Serial.println(" m/s^2");
+  Serial.print  ("Resolution:   "); Serial.print(accelsensor.resolution); Serial.println(" m/s^2");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+
+}
+void displayDataRate(void)
+{
+  Serial.print  ("Data Rate:    "); 
+  
+  switch(accel.getDataRate())
+  {
+    case ADXL345_DATARATE_3200_HZ:
+      Serial.print  ("3200 "); 
+      break;
+    case ADXL345_DATARATE_1600_HZ:
+      Serial.print  ("1600 "); 
+      break;
+    case ADXL345_DATARATE_800_HZ:
+      Serial.print  ("800 "); 
+      break;
+    case ADXL345_DATARATE_400_HZ:
+      Serial.print  ("400 "); 
+      break;
+    case ADXL345_DATARATE_200_HZ:
+      Serial.print  ("200 "); 
+      break;
+    case ADXL345_DATARATE_100_HZ:
+      Serial.print  ("100 "); 
+      break;
+    case ADXL345_DATARATE_50_HZ:
+      Serial.print  ("50 "); 
+      break;
+    case ADXL345_DATARATE_25_HZ:
+      Serial.print  ("25 "); 
+      break;
+    case ADXL345_DATARATE_12_5_HZ:
+      Serial.print  ("12.5 "); 
+      break;
+    case ADXL345_DATARATE_6_25HZ:
+      Serial.print  ("6.25 "); 
+      break;
+    case ADXL345_DATARATE_3_13_HZ:
+      Serial.print  ("3.13 "); 
+      break;
+    case ADXL345_DATARATE_1_56_HZ:
+      Serial.print  ("1.56 "); 
+      break;
+    case ADXL345_DATARATE_0_78_HZ:
+      Serial.print  ("0.78 "); 
+      break;
+    case ADXL345_DATARATE_0_39_HZ:
+      Serial.print  ("0.39 "); 
+      break;
+    case ADXL345_DATARATE_0_20_HZ:
+      Serial.print  ("0.20 "); 
+      break;
+    case ADXL345_DATARATE_0_10_HZ:
+      Serial.print  ("0.10 "); 
+      break;
+    default:
+      Serial.print  ("???? "); 
+      break;
+  }  
+  Serial.println(" Hz");  
+}
+void displayRange(void)
+{
+  Serial.print  ("Range:         +/- "); 
+  
+  switch(accel.getRange())
+  {
+    case ADXL345_RANGE_16_G:
+      Serial.print  ("16 "); 
+      break;
+    case ADXL345_RANGE_8_G:
+      Serial.print  ("8 "); 
+      break;
+    case ADXL345_RANGE_4_G:
+      Serial.print  ("4 "); 
+      break;
+    case ADXL345_RANGE_2_G:
+      Serial.print  ("2 "); 
+      break;
+    default:
+      Serial.print  ("?? "); 
+      break;
+  }  
+  Serial.println(" g");  
+}
+/**************************************************************************/
+/*
+    Arduino setup function (automatically called at startup)
+*/
+/**************************************************************************/
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.println("Pressure Sensor Test"); Serial.println("");
+  Serial.println("Accelerometer Test"); Serial.println("");
+
+  /* Initialise the sensor */
+  if(!bmp.begin())
+  {
+    /* There was a problem detecting the BMP085 ... check your connections */
+    Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+  accel.setRange(ADXL345_RANGE_16_G);
+  displayDataRate();
+  displayRange();
+  Serial.println("");
+}
+ 
+/**************************************************************************/
+/*
+    Arduino loop function, called once 'setup' is complete (your own code
+    should go here)temperature                                                                
+*/
+/**************************************************************************/
+void loop(void) 
+{
+  /* Get a new sensor event */ 
+  sensors_event_t bmpevent;
+  bmp.getEvent(&bmpevent);
+  sensors_event_t accelevent; 
+  accel.getEvent(&accelevent);
+  /* Display the results (barometric pressure is measure in hPa) */
+  if (bmpevent.pressure)
+  {
+    /* Display atmospheric pressue in hPa */
+    Serial.print("Pressure:    ");
+    Serial.print(bmpevent.pressure);
+    Serial.println(" hPa");
+    
+    /* Calculating altitude with reasonable accuracy requires pressure    *
+     * sea level pressure for your position at the moment the data is     *
+     * converted, as well as the ambient temperature in degress           *
+     * celcius.  If you don't have these values, a 'generic' value of     *
+     * 1013.25 hPa can be used (defined as SENSORS_PRESSURE_SEALEVELHPA   *
+     * in sensors.h), but this isn't ideal and will give variable         *
+     * results from one day to the next.                                  *
+     *                                                                    *
+     * You can usually find the current SLP value by looking at weather   *
+     * websites or from environmental information centers near any major  *
+     * airport.                                                           *
+     *                                                                    *
+     * For example, for Paris, France you can check the current mean      *
+     * pressure and sea level at: http://bit.ly/16Au8ol                   */
+     
+    /* First we get the current temperature from the BMP085 */
+    float temperature;
+    bmp.getTemperature(&temperature);
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" C");
+
+    /* Then convert the atmospheric pressure, and SLP to altitude         */
+    /* Update this next line with the current SLP for better results      */
+    float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+    Serial.print("Altitude:    "); 
+    Serial.print(bmp.pressureToAltitude(seaLevelPressure,
+                                        bmpevent.pressure)); 
+    Serial.println(" m");
+    Serial.println("");
+  }
+  else
+  {
+    Serial.println("Sensor error");
+  }
+  float xaccel = accelevent.acceleration.x + xoffset;
+  float yaccel = accelevent.acceleration.y + yoffset;
+  float zaccel = accelevent.acceleration.z + zoffset;
+  Serial.print("X: "); Serial.print(accelevent.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(yaccel); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(zaccel); Serial.print("  ");Serial.println("m/s^2 ");
+  
+  delay(1000);
+}
+
+
+
+
