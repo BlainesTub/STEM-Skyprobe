333021e7741cba06c1ed1c106c505477c2bc9a26
Lukas Vann Struth <lvannstruth@gmail.com>
2017-10-24 13:00:14 -0700

    Downsized skyprobetest so it will compile when more code is added

--- Test Sketch/skyprobetest/skyprobetest.ino
+++ Test Sketch/skyprobetest/skyprobetest.ino
@@ -128,119 +128,6 @@ void isMoving(float exaccel, float eyaccel, float ezaccel) {
   
 }
 
-//These functions display useful information about the sensors and are useful for debugging. 
-void displaySensorDetails(void)
-{
-  sensor_t bmpsensor;
-  bmp.getSensor(&bmpsensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(bmpsensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(bmpsensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(bmpsensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(bmpsensor.max_value); Serial.println(" hPa");
-  Serial.print  ("Min Value:    "); Serial.print(bmpsensor.min_value); Serial.println(" hPa");
-  Serial.print  ("Resolution:   "); Serial.print(bmpsensor.resolution); Serial.println(" hPa");  
-  Serial.println("------------------------------------");
-  Serial.println("");
-  sensor_t accelsensor;
-  accel.getSensor(&accelsensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(accelsensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(accelsensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(accelsensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(accelsensor.max_value); Serial.println(" m/s^2");
-  Serial.print  ("Min Value:    "); Serial.print(accelsensor.min_value); Serial.println(" m/s^2");
-  Serial.print  ("Resolution:   "); Serial.print(accelsensor.resolution); Serial.println(" m/s^2");  
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-
-}
-
-void displayDataRate(void)
-{
-  Serial.print  ("Data Rate:    "); 
-  
-  switch(accel.getDataRate())
-  {
-    case ADXL345_DATARATE_3200_HZ:
-      Serial.print  ("3200 "); 
-      break;
-    case ADXL345_DATARATE_1600_HZ:
-      Serial.print  ("1600 "); 
-      break;
-    case ADXL345_DATARATE_800_HZ:
-      Serial.print  ("800 "); 
-      break;
-    case ADXL345_DATARATE_400_HZ:
-      Serial.print  ("400 "); 
-      break;
-    case ADXL345_DATARATE_200_HZ:
-      Serial.print  ("200 "); 
-      break;
-    case ADXL345_DATARATE_100_HZ:
-      Serial.print  ("100 "); 
-      break;
-    case ADXL345_DATARATE_50_HZ:
-      Serial.print  ("50 "); 
-      break;
-    case ADXL345_DATARATE_25_HZ:
-      Serial.print  ("25 "); 
-      break;
-    case ADXL345_DATARATE_12_5_HZ:
-      Serial.print  ("12.5 "); 
-      break;
-    case ADXL345_DATARATE_6_25HZ:
-      Serial.print  ("6.25 "); 
-      break;
-    case ADXL345_DATARATE_3_13_HZ:
-      Serial.print  ("3.13 "); 
-      break;
-    case ADXL345_DATARATE_1_56_HZ:
-      Serial.print  ("1.56 "); 
-      break;
-    case ADXL345_DATARATE_0_78_HZ:
-      Serial.print  ("0.78 "); 
-      break;
-    case ADXL345_DATARATE_0_39_HZ:
-      Serial.print  ("0.39 "); 
-      break;
-    case ADXL345_DATARATE_0_20_HZ:
-      Serial.print  ("0.20 "); 
-      break;
-    case ADXL345_DATARATE_0_10_HZ:
-      Serial.print  ("0.10 "); 
-      break;
-    default:
-      Serial.print  ("???? "); 
-      break;
-  }  
-  Serial.println(" Hz");  
-}
-void displayRange(void)
-{
-  Serial.print  ("Range:         +/- "); 
-  
-  switch(accel.getRange())
-  {
-    case ADXL345_RANGE_16_G:
-      Serial.print  ("16 "); 
-      break;
-    case ADXL345_RANGE_8_G:
-      Serial.print  ("8 "); 
-      break;
-    case ADXL345_RANGE_4_G:
-      Serial.print  ("4 "); 
-      break;
-    case ADXL345_RANGE_2_G:
-      Serial.print  ("2 "); 
-      break;
-    default:
-      Serial.print  ("?? "); 
-      break;
-  }  
-  Serial.println(" g");  
-}
 
 void setup(void) 
 {
@@ -309,10 +196,7 @@ void setup(void)
   }
   
   /* Display some basic information on the sensors */
-  displaySensorDetails();
   accel.setRange(ADXL345_RANGE_16_G);
-  displayDataRate();
-  displayRange();
   Serial.println("");
 }
 
